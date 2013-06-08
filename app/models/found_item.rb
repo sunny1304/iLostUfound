b639731608e6a71f529a328d64ecba91b4506255
sunny1304 <farhan@horoppa.com>
2013-06-09 00:34:47 +0600

    added model validation and happy.js validation for email for both lost and found

--- app/models/found_item.rb
+++ app/models/found_item.rb
@@ -1,5 +1,7 @@
 class FoundItem < ActiveRecord::Base
   attr_accessible :additional_contact, :address, :cell, :email, :found_date, :found_item, :found_item_pic, :found_location, :name,:longitude,:latitude
+  validates_presence_of :name, :address, :cell, :found_item, :found_location, :found_date, :email
+
   geocoded_by :found_location
   after_validation :geocode, :if => :found_location_changed?
   mount_uploader  :found_item_pic, FoundItemPicUploader
--- app/models/lost_item.rb
+++ app/models/lost_item.rb
@@ -1,5 +1,7 @@
 class LostItem < ActiveRecord::Base
   attr_accessible :additional_contact, :address, :cell, :email, :lost_date, :lost_item, :lost_item_pic, :lost_location, :name, :latitude, :longitude
+  validates_presence_of :name, :address, :cell, :lost_item, :lost_location, :lost_date, :email
+
   geocoded_by :lost_location
   after_validation :geocode, :if => :lost_location_changed?
   mount_uploader  :lost_item_pic, LostItemPicUploader

ad3f89a43df735a60511735b8d33d806124db786
sunny1304 <farhan@horoppa.com>
2013-06-08 17:43:30 +0600

    added helper function to process the output

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
 		vowels = %w(a e i o u)
 		(vowels.include?(name.to_s.first.downcase))? "an" : "a"
 	end
+
+	def process_item_name(lost_item)
+		lost_item_array = lost_item.split(" ")
+		if lost_item_array.count > 1
+			first_word,*rest = lost_item_array
+			(%w(a an).include?(first_word))? rest.join(" ") : lost_item
+		else
+			lost_item
+		end
+	end
 end

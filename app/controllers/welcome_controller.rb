4ebecf3a5c3b5179334074744bdc3bd882aa8c20
sunny1304 <farhan@horoppa.com>
2013-06-09 22:07:18 +0600

    changed welcomw view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,10 @@ class WelcomeController < ApplicationController
   def index
   	visitor_ip = request.ip
   	@lost_report_count = LostItem.where("ip_address=?",visitor_ip).pluck(:id).count
-  	@lost_item_ids = LostItem.where("ip_address=?",visitor_ip).pluck(:id) if @lost_report_count > 0
+  	@lost_item_ids = LostItem.where("ip_address=?",visitor_ip).select([:id,:lost_item]) if @lost_report_count > 0
 
   	@found_report_count = FoundItem.where("ip_address=?",visitor_ip).pluck(:id).count
-  	@found_item_ids = FoundItem.where("ip_address=?",visitor_ip).pluck(:id) if @found_report_count > 0
+  	@found_item_ids = FoundItem.where("ip_address=?",visitor_ip).select([:id,:found_item]) if @found_report_count > 0
 
   end
 

98d49dfa1678c7e26f03f7cccb18b8e697fa1772
sunny1304 <farhan@horoppa.com>
2013-06-13 19:59:17 +0600

    set mail delivery error true

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -29,7 +29,7 @@ ILostUfound::Application.configure do
   # ActionMailer::Base.deliveries array.
   # config.action_mailer.delivery_method = :test
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",

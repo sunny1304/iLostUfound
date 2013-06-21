6ebc6152529d787ecd3dedbd54c19a5193e28e44
sunny1304 <sunny1304.ad1@gmail.com>
2013-06-21 11:56:22 +0600

    added found item status for search functions

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,7 @@ class SearchController < ApplicationController
       # @lost_items = @search.results
       # # logger.debug "_________#{@search.inspect}_________"
 
-      @lost_items = LostItem.where("(lost_item ILIKE ? OR lost_location ILIKE ?) AND user_id IS NOT NULL","%#{params[:search]}%","%#{params[:search]}%").page(params[:page]).per(10)
+      @lost_items = LostItem.where("(lost_item ILIKE ? OR lost_location ILIKE ?) AND user_id IS NOT NULL","%#{params[:search]}%","%#{params[:search]}%").not_found.order("id desc").page(params[:page]).per(10)
     end
 
   	respond_to do |format|
@@ -21,7 +21,7 @@ class SearchController < ApplicationController
   def found
     if params[:search].present?
       search_term = params[:search]
-      @found_items = FoundItem.where("(found_item ILIKE ? OR found_location ILIKE ?) AND user_id IS NOT NULL","%#{search_term}%","%#{search_term}%").page(params[:page]).per(10)
+      @found_items = FoundItem.where("(found_item ILIKE ? OR found_location ILIKE ?) AND user_id IS NOT NULL","%#{search_term}%","%#{search_term}%").not_found.order("id desc").page(params[:page]).per(10)
     end
     respond_to do |format|
       format.js
@@ -33,7 +33,7 @@ class SearchController < ApplicationController
   def suggestion
     if params[:id].present?
       found_item = FoundItem.find(params[:id].to_i)
-      @suggestion_items = LostItem.where("(lost_item ILIKE ? OR lost_location ILIKE ?) AND user_id IS NOT NULL","%#{found_item.found_item}%", "%#{found_item.found_location}%")
+      @suggestion_items = LostItem.where("(lost_item ILIKE ? OR lost_location ILIKE ?) AND user_id IS NOT NULL","%#{found_item.found_item}%", "%#{found_item.found_location}%").not_found.order("id desc")
       @notice = params[:notice] if params[:notice].present?
     end
   end
